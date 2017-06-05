50ccc722f5d02cc0030b87594f1dffdfb182020e
Macarena Baltra <macarena.bc2011@gmail.com>
2017-06-04 20:05:16 -0400

    Primeros cödigos Js - Scroll Menú

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// Efecto para la bajada del menú
+	var menuScroll = document.getElementById("menuConJs"); // conectamos con el id del nav
+	var altura = menuScroll.offsetTop;// propiedad que muestra la distancia que desde el inicio de la pagina hasta el menú. Se puede ver comnnnnnn (alert(menuScroll.offsetTop);)
+	alert(menuScroll.offsetTop);
+
+	//Evento
+	window.addEventListener("scroll", function(){
+		//Condiciones para que se aplique
+		if(){
+
+		}
+	});
+	
