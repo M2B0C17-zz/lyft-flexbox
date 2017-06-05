fd5673bda55b1dc346c853c5236847e5d98c10e0
Macarena Baltra <macarena.bc2011@gmail.com>
2017-06-04 23:23:04 -0400

    Subiendo seccion footer (no se quieren ver los iconos u.u),  base de seccion videos y informacion1

--- js/app.js
+++ js/app.js
@@ -6,10 +6,12 @@ var menuScroll = document.getElementById("menuConJs"); // conectamos con el id d
 		"use strict";
 
 		//Condiciones para que se aplique
-		if(document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280){ // si la cantidad de scroll es mayor a la altura del inicio, se le aplique la clase fixed
-			menuScroll.classList.add("scroll");
+		if(document.body.scrollTop >= 30){ 
+			menuScroll.classList.add("menuColor");
+			menuScroll.classList.remove("menuSinColors");
 		}else {
-			menuScroll.classList.remove("scroll"); // para que cuando sea menor a la altura del inicio, se quite.
+			menuScroll.classList.add("menuSinColors");
+			menuScroll.classList.remove("menuColor");
 		}
 	};	
 
@@ -22,18 +24,19 @@ var menuScroll = document.getElementById("menuConJs"); // conectamos con el id d
 	//Evento
 	window.addEventListener("scroll", function(){
 		//Condiciones para que se aplique
-		if(window.pageYOffset > altura){ // si la cantidad de scroll es mayor a la altura del inicio, se le aplique la clase fixed
+		if(window.pageYOffset > altura){ 
 			menuScroll.classList.add("fixed");
 		}else {
 			menuScroll.classList.remove("fixed"); // para que cuando sea menor a la altura del inicio, se quite.
 		}
 	});
 	
-*/
+
 
 // Código de apoyo
-/*
+
 var altura = menuScroll.offsetTop;// propiedad que muestra la distancia que desde el inicio de la pagina hasta el menú. Se puede ver comnnnnnn (alert(menuScroll.offsetTop);)
 alert(menuScroll.offsetTop);
 
 alert(menuScroll.pageYOffset); // para calcular la altura en que queremos que comience el Scroll
+*/
