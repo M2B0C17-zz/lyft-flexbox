6c5ccd1a946eb27c7bb0d28d0357d764548933dd
Macarena Baltra <macarena.bc2011@gmail.com>
2017-06-04 20:52:54 -0400

    Intentos fallidos de js para el cambio de estilos al hacer scroll con el menú

--- js/app.js
+++ js/app.js
@@ -1,13 +1,19 @@
-// Efecto para la bajada del menú
+// Efecto para el scroll del menú
 	var menuScroll = document.getElementById("menuConJs"); // conectamos con el id del nav
-	var altura = menuScroll.offsetTop;// propiedad que muestra la distancia que desde el inicio de la pagina hasta el menú. Se puede ver comnnnnnn (alert(menuScroll.offsetTop);)
-	alert(menuScroll.offsetTop);
-
+	
 	//Evento
 	window.addEventListener("scroll", function(){
 		//Condiciones para que se aplique
-		if(){
-
+		if(window.pageYOffset > altura){ // si la cantidad de scroll es mayor a la altura del inicio, se le aplique la clase fixed
+			menuScroll.classList.add("fixed");
 		}
+
 	});
 	
+
+// Código de apoyo
+/*
+var altura = menuScroll.offsetTop;// propiedad que muestra la distancia que desde el inicio de la pagina hasta el menú. Se puede ver comnnnnnn (alert(menuScroll.offsetTop);)
+alert(menuScroll.offsetTop);
+
+alert(menuScroll.pageYOffset); // para calcular la altura en que queremos que comience el Scroll
