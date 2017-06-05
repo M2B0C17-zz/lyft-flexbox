572467a51d68c9e6f6137cd47436d7248bcce516
Macarena Baltra <macarena.bc2011@gmail.com>
2017-06-04 21:25:55 -0400

    Cambios en footer - ultimo intento en js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,22 @@
 // Efecto para el scroll del menú
+var menuScroll = document.getElementById("menuConJs"); // conectamos con el id del nav
+	
+	//Evento
+	window.onscroll = function(){
+		"use strict";
+
+		//Condiciones para que se aplique
+		if(document.body.scrollTop >= 280 || document.documentElement.scrollTop >= 280){ // si la cantidad de scroll es mayor a la altura del inicio, se le aplique la clase fixed
+			menuScroll.classList.add("scroll");
+		}else {
+			menuScroll.classList.remove("scroll"); // para que cuando sea menor a la altura del inicio, se quite.
+		}
+	};	
+
+
+
+/* Intentos - Fallidos
+
 	var menuScroll = document.getElementById("menuConJs"); // conectamos con el id del nav
 	
 	//Evento
@@ -11,6 +29,7 @@
 		}
 	});
 	
+*/
 
 // Código de apoyo
 /*
